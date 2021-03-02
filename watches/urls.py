b4af3dbdb35e7a4caa6fb5473a1ba4eefb51e546
ElJefe111944 <jamesfarr1@sky.com>
2021-03-02 15:36:37 +0000

    Watch_details views and urls added

--- watches/urls.py
+++ watches/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_watches, name='watches'),
+    path('<watch_id>', views.watch_detail, name='watch_detail'),
 ]
--- watches/views.py
+++ watches/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Watch
 
 # Create your views here.
 
 
 def all_watches(request):
-    """  A view to all watches including any sorting and search queries  """
+    """  A view to show an individual watch  """
    
     watches = Watch.objects.all()
 
@@ -14,3 +14,15 @@ def all_watches(request):
     }
    
     return render(request, 'watches/all_watches.html', context)
+
+
+def watch_detail(request, watch_id):
+    """  A view to all watches including any sorting and search queries  """
+   
+    watch = get_object_or_404(Watch, pk=watch_id)
+
+    context = {
+        'watch': watch,
+    }
+   
+    return render(request, 'watches/watch_detail.html', context)
