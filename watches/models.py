a51df2ecf0da02fc16f4fa8a16a1b83c6c8c9196
ElJefe111944 <jamesfarr1@sky.com>
2021-03-03 07:28:22 +0000

    Meta option changed on category. Category field added to watch model

--- watches/models.py
+++ watches/models.py
@@ -45,6 +45,8 @@ class Watch(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     brand = models.ForeignKey(
         'Brand', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
