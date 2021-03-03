82c084a3ea0536f3e87d29fd7a2e1dbf91373daa
ElJefe111944 <jamesfarr1@sky.com>
2021-03-03 17:54:11 +0000

    Sorting by price, brand and rating added

--- watches/views.py
+++ watches/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Watch, Category
+from django.db.models.functions import Lower
 
 # Create your views here.
 
@@ -12,8 +13,23 @@ def all_watches(request):
     watches = Watch.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                watches = watches.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            watches = watches.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             watches = watches.filter(category__name__in=categories)
@@ -30,10 +46,13 @@ def all_watches(request):
                 name__icontains=query) | Q(description__icontains=query)
             watches = watches.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'watches': watches,
         'search_term': query,
-        'current_categories': categories, 
+        'current_categories': categories,
+        'current_sorting': current_sorting, 
     }
    
     return render(request, 'watches/all_watches.html', context)
