c28dd8ea523a384e43b79546b44a43bf4fe65ae6
ElJefe111944 <jamesfarr1@sky.com>
2021-03-05 08:07:32 +0000

    templates folder and shopping cart template created

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view_shopping_cart(request):
+    """  A view to render the shopping cart  """
+    
+    return render(request, 'shopping_cart/shopping_cart.html')
