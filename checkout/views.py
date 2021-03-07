cb9b9a415b198628e2d35058275a5e174a6d2431
ElJefe111944 <jamesfarr1@sky.com>
2021-03-07 17:00:39 +0000

    Checkout urls and templates created

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+def checkout(request):
+    shoppingcart = request.session.get('shoppingcart', {})
+    if not shoppingcart:
+        messages.error(request, "There is nothing currently in your shopping bag")
+        return redirect(reverse('watches'))
+    
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
--- timekeepers/urls.py
+++ timekeepers/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('homepage.urls')),
     path('all_watches/', include('watches.urls')),
     path('shoppingcart/', include('shoppingcart.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
