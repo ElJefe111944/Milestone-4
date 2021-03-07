68c558bc8a339a787ae376d8f31612b9ee4ff53c
ElJefe111944 <jamesfarr1@sky.com>
2021-03-07 18:06:26 +0000

    Crispy forms installed. Checkout template content added

--- checkout/admin.py
+++ checkout/admin.py
@@ -57,5 +57,4 @@ class OrderAdmin(admin.ModelAdmin):
     ordering = ('-date',)
 
 
-admin.site.register(Order, OrderAdmin)
-    
+admin.site.register(Order, OrderAdmin)    
--- checkout/forms.py
+++ checkout/forms.py
@@ -23,31 +23,30 @@ class OrderForm(forms.ModelForm):
             )
 
     def __init__(self, *args, **kwargs):
-            """
-            Add placeholders and classes, remove auto-generated
-            labels and set autofocus on first field
-            """
-            super().__init__(*args, **kwargs)
-            placeholders = {                
-                'first_name': 'First Name',
-                'last_name': 'Last Name', 
-                'email': 'Email',
-                'phone_number': 'Phone Number', 
-                'country': 'Country', 
-                'postcode': 'Postcode',
-                'address_line1':' Address 1',
-                'address_line2': 'Address 2', 
-                'town_or_city': 'Town or City', 
-                'state_or_county': 'State or County',
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {                           
+            'first_name': 'First Name',
+            'last_name': 'Last Name', 
+            'email': 'Email',
+            'phone_number': 'Phone Number', 
+            'country': 'Country', 
+            'postcode': 'Postcode',
+            'address_line1': 'Address 1',
+            'address_line2': 'Address 2', 
+            'town_or_city': 'Town or City', 
+            'state_or_county': 'State or County',
             }
 
-    self.fields['first_name'].widget.attrs['autofocus'] = True
-    self.fields['last_name'].widget.attrs['autofocus'] = True
-    for field in self.fields:
-        if self.fields[field].required:
-            placeholder = f'{placeholders[field]} *'
-        else:
-            placeholder = placeholders[field]
-        self.fields[field].widget.attrs['placeholder'] = placeholder
-        self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-        self.fields[field].label = False
+        self.fields['first_name'].widget.attrs['autofocus'] = True    
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
