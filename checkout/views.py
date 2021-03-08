1aa0cb8517ba8192d017242a7a190fc008e9d8af
ElJefe111944 <jamesfarr1@sky.com>
2021-03-08 21:11:23 +0000

    checkout ccs moved to base css file

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51ISpcuFD3z975eMdp4FlRPhOi294WwDqphFoj8pPdEpPhMDSVAgJT20HfabXRbIX0B94XA0rrUlktKJxFJ918mVg00sIuBIT5a',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
