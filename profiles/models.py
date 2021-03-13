061d4dce4b7b7f9d1bd83c7c051ffe605235508e
ElJefe111944 <jamesfarr1@sky.com>
2021-03-13 15:16:15 +0000

    Updated form added to profile template

--- profiles/models.py
+++ profiles/models.py
@@ -14,11 +14,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(
-        max_length=20, null=True, blank=True)
-    default_country = CountryField(
-        blank_label="Country *", null=True, blank=True)
-    default_postcode = models.CharField(
-        max_length=20, null=True, blank=True)
+        max_length=20, null=True, blank=True)    
     default_address_line1 = models.CharField(
         max_length=80, null=True, blank=True)
     default_address_line2 = models.CharField(
@@ -27,6 +23,11 @@ class UserProfile(models.Model):
         max_length=40, null=True, blank=True)
     default_state_or_county = models.CharField(
         max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(
+        max_length=20, null=True, blank=True)
+    default_country = CountryField(
+        blank_label="Country *", null=True, blank=True)
+
 
     def __str__(self):
         return self.user.username
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
+
 
 def profile(request):
     """ 
@@ -8,8 +10,13 @@ def profile(request):
     User's profile page 
     """
     profile = get_object_or_404(UserProfile, user=request.user)
+    orders = profile.orders.all()
+
+    form = UserProfileForm(instance=profile)
+    
     template = "profiles/profile.html"
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
     return render(request, template, context)
