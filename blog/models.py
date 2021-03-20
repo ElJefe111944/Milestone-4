980319878c711352fb19660c757dea2fb1549303
ElJefe111944 <jamesfarr1@sky.com>
2021-03-20 11:02:29 +0000

    Blog view, model and template created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from profiles.models import UserProfile
+
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=55)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    body = models.TextField(max_length=255)
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='homepage'),
+    path('', views.blog, name='blog'),
 ]
