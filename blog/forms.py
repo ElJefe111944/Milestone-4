3d4fa08e49560c92a3ee587eb9b3be361cc70eb9
ElJefe111944 <jamesfarr1@sky.com>
2021-03-21 16:01:40 +0000

    Blog category functionality created

--- blog/forms.py
+++ blog/forms.py
@@ -1,22 +1,31 @@
 from django import forms
 from watches.widgets import (
     CustomClearableFileInput)
-from .models import Post
+from .models import Post, Category
+
+choices = Category.objects.all().values_list('name', 'name')
+
+choice_list = []
+
+for item in choices:
+    choice_list.append(item)
 
 
-class PostForm(forms.ModelForm):
+class PostForm(forms.ModelForm):   
         
     class Meta:
         model = Post
         fields = [
-            'title', 'author', 'title_tag', 'blog_image', 'subheading1',
-            'main_content1', 'subheading2', 'main_content2'
+            'title', 'author', 'category', 'title_tag', 'blog_image',
+            'subheading1', 'main_content1', 'subheading2', 'main_content2'
             ] 
 
         widgets = {
                 'title': forms.TextInput(
                     attrs={'class': 'form-control', 'placeholder': 'Blog Title'}),
                 # 'author': forms.Select(attrs={'class': 'form-control'}),
+                'category': forms.Select(
+                    choices=choice_list, attrs={'class': 'form-control'}),
                 'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
                 'subheading1': forms.TextInput(attrs={'class': 'form-control'}),
                 'main_content1': forms.Textarea(attrs={'class': 'form-control'}),
@@ -30,14 +39,16 @@ class EditForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = [
-            'title', 'author', 'title_tag', 'blog_image', 'subheading1',
+            'title', 'author', 'category', 'title_tag', 'blog_image', 'subheading1',
             'main_content1', 'subheading2', 'main_content2'
             ] 
 
         widgets = {
                 'title': forms.TextInput(
                     attrs={'class': 'form-control', 'placeholder': 'Blog Title'}),
-                'author': forms.Select(attrs={'class': 'form-control'}),
+                # 'author': forms.Select(attrs={'class': 'form-control'}),
+                'category': forms.Select(
+                    choices=choice_list, attrs={'class': 'form-control'}),
                 'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
                 'subheading1': forms.TextInput(attrs={'class': 'form-control'}),
                 'main_content1': forms.Textarea(attrs={'class': 'form-control'}),
