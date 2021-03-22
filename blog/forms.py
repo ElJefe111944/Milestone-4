8c30df8f485cc6eaddb4f3983e4bea402521102d
ElJefe111944 <jamesfarr1@sky.com>
2021-03-22 22:15:33 +0000

    Blog details page redesigned

--- blog/forms.py
+++ blog/forms.py
@@ -1,14 +1,5 @@
 from django import forms
-from watches.widgets import (
-    CustomClearableFileInput)
-from .models import Post, Category
-
-choices = Category.objects.all().values_list('name', 'name')
-
-choice_list = []
-
-for item in choices:
-    choice_list.append(item)
+from .models import Post
 
 
 class PostForm(forms.ModelForm):   
@@ -16,16 +7,14 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = [
-            'title', 'author', 'info',  'category', 'title_tag', 'blog_image',
+            'title', 'author', 'info','title_tag', 'blog_image',
             'subheading1', 'main_content1', 'subheading2', 'main_content2'
             ] 
 
         widgets = {
                 'title': forms.TextInput(
                     attrs={'class': 'form-control', 'placeholder': 'Blog Title'}),
-                # 'author': forms.Select(attrs={'class': 'form-control'}),
-                'category': forms.Select(
-                    choices=choice_list, attrs={'class': 'form-control'}),
+                # 'author': forms.Select(attrs={'class': 'form-control'}),                
                 'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
                 'info': forms.TextInput(attrs={'class': 'form-control'}),
                 'subheading1': forms.TextInput(attrs={'class': 'form-control'}),
@@ -40,16 +29,14 @@ class EditForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = [
-            'title', 'author', 'category', 'title_tag', 'info', 'blog_image', 'subheading1',
+            'title', 'author', 'title_tag', 'info', 'blog_image', 'subheading1',
             'main_content1', 'subheading2', 'main_content2'
             ] 
 
         widgets = {
                 'title': forms.TextInput(
                     attrs={'class': 'form-control', 'placeholder': 'Blog Title'}),
-                # 'author': forms.Select(attrs={'class': 'form-control'}),
-                'category': forms.Select(
-                    choices=choice_list, attrs={'class': 'form-control'}),
+                # 'author': forms.Select(attrs={'class': 'form-control'}),               
                 'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
                 'info': forms.TextInput(attrs={'class': 'form-control'}),
                 'subheading1': forms.TextInput(attrs={'class': 'form-control'}),
--- blog/models.py
+++ blog/models.py
@@ -21,16 +21,16 @@ class Post(models.Model):
     publish_date = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=55)
     title_tag = models.CharField(max_length=55)
-    info = models.CharField(max_length=55, default="Summary")
+    info = models.CharField(max_length=155, default="Summary")
     author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     blog_image = models.ImageField(blank=True)
     subheading1 = models.TextField(
         max_length=55, null=False, blank=False, default="Subheading")
     main_content1 = models.TextField(
-        max_length=255, null=False, blank=False, default="Main Content")
+        max_length=355, null=False, blank=False, default="Main Content")
     subheading2 = models.TextField(max_length=55, null=True, blank=True)
-    main_content2 = models.TextField(max_length=255, null=True, blank=True)
-    category = models.CharField(max_length=55)
+    main_content2 = models.TextField(max_length=355, null=True, blank=True)
+
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
