20e8d5b7b5c3078895190e94b50456e98fb002db
ElJefe111944 <jamesfarr1@sky.com>
2021-03-27 11:43:47 +0000

    Urls for edit and delete blogs added to blog templates

--- watches/views.py
+++ watches/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
-from .models import Watch, Category, Brand
+from .models import Watch, Category
 from .forms import ProductForm
 
 # Create your views here.
