8e1f4827ee58f55105779c69aa0426a468d7730e
ElJefe111944 <jamesfarr1@sky.com>
2021-03-28 10:35:47 +0000

    Removed secret key and set debug

--- timekeepers/settings.py
+++ timekeepers/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4i=8iq0^0bht0m6gwl3wuplz0cd$$*rxcdqnq2awpje@te54%h'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['eljefe111944-timekeepers.herokuapp.com', 'localhost']
 
