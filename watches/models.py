d0b54cc48295006bd31041f92759ca364e36396f
ElJefe111944 <jamesfarr1@sky.com>
2021-03-29 11:49:13 +0000

    Review model created and migrated

--- watches/models.py
+++ watches/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
 
@@ -52,10 +54,27 @@ class Watch(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     discount = models.FloatField(blank=True, null=True)
     label = models.CharField(
-        choices=LABEL, max_length=20, default='SOME STRING')
+        choices=LABEL, max_length=20, default='SOME STRING', blank=True, null=True)
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    watch = models.ForeignKey(
+        Watch,
+        related_name='reviews',
+        on_delete=models.CASCADE
+    )
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+    review = models.CharField(max_length=255)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.review
